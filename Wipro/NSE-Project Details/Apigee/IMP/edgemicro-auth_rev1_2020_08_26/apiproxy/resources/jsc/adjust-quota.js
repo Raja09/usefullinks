ae5b6157770fd421acda023c217fdf588e01a74c
Raja Sekhar <rajasekhardi@gmail.com>
2020-09-22 14:58:32 +0530

    removed unwanted files

--- Wipro/NSE-Project Details/Apigee/IMP/edgemicro-auth_rev1_2020_08_26/apiproxy/resources/jsc/adjust-quota.js
+++
@@ -1,4 +0,0 @@
-var debugMpId = context.getVariable("debug.mp.id");
-if(debugMpId) {
-        context.setVariable("debugMpId", ",\n\"debugMpId\" : \""+ context.getVariable("system.uuid")+ "\"");
-}
--- Wipro/NSE-Project Details/Apigee/IMP/edgemicro-auth_rev1_2020_08_26/apiproxy/resources/jsc/set-quota-variables.js
+++
@@ -1,7 +0,0 @@
- 
-context.setVariable('quota.identifier', request.body.asJSON.identifier);
-context.setVariable("quota.allow",request.body.asJSON.allow);
-context.setVariable("quota.interval",request.body.asJSON.interval);
-context.setVariable("quota.unit",request.body.asJSON.timeUnit);
-context.setVariable("quota.weight",request.body.asJSON.weight);
-context.setVariable("debug.mp.id", request.body.asJSON.debugMpId); 
