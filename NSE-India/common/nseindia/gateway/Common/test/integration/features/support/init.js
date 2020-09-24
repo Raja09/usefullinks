b1e3ce7aac30651466afe50e00732e8294a79595
Dirisam Rajasekhar (INDIA - BAS) <DI40055325@wipro.com>
2020-09-24 18:27:00 +0530

    files are deleted

--- NSE-India/common/nseindia/gateway/Common/test/integration/features/support/init.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const apickli = require('apickli');
-const {Before} = require('cucumber');
-
-Before(function() {
-    this.apickli = new apickli.Apickli('http', 'httpbin.org');
-    this.apickli.addRequestHeader('Cache-Control', 'no-cache');
-});
